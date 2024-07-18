f3564da645e8301abb7d614a475eb192a122bb21
emartinez <enri.fc.98@gmail.com>
2024-07-18 22:04:29 +0200

    Finish it

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,37 @@
 import Calculator from "./components/Calculator.vue";
 </script>
 <template>
-  <main>
-    <Calculator />
-  </main>
+  <div>
+    <main>
+      <div class="calculator-heading">
+        <h1>Calculadora Ley de Gay-Lussac</h1>
+      </div>
+      <Calculator />
+      <div class="footer">
+        <p>Hecho por Enrique para mi primo Juan © 2024</p>
+      </div>
+    </main>
+  </div>
 </template>
+<style #scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  max-width: 820px;
+  margin: auto;
+}
+
+.calculator-heading {
+  text-align: center;
+  padding: 0 4rem;
+  font-size: 1.1rem;
+}
+
+h1 {
+  color: black;
+  font-weight: bold;
+}
+</style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,6 +2,7 @@
 import { ref, computed } from "vue"
 
 import InputNumber from "primevue/inputnumber"
+import Card from 'primevue/card'
 
 const fahrenheit = 273.15
 
@@ -24,40 +25,55 @@ const diferencia_presion = computed(() =>
 
 <template>
   <div class="datos-calculo">
-    <div>
-      <label for="presion_inicial">Presión Inicial</label>
-      <InputNumber id="presion_inicial" v-model="presion_inicial" suffix=" Bar." fluid></InputNumber>
-
-      <label for="temperatura_inicial">Temperatura Inicial</label>
-      <InputNumber id="temperatura_inicial" v-model="temperatura_inicial" suffix=" ºC" fluid></InputNumber>
-
-      <label for="temperatura_final">Temperatura Final</label>
-      <InputNumber id="temperatura_final" v-model="temperatura_final" suffix=" ºC" fluid></InputNumber>
-
-      <label for="presion_final">Presion Final</label>
-      <InputNumber id="presion_final" :model-value="presion_final" :readonly="true" suffix=" Bar." fluid></InputNumber>
-    </div>
-    <div>
-      <label for="diferencia_temperatura">Diferencia Temperatura</label>
-      <InputNumber id="diferencia_temperatura" :model-value="diferencia_temperatura" :readonly="true" suffix=" ºC"
-        fluid></InputNumber>
-
-      <label for="diferencia_presion">Diferencia Presión</label>
-      <InputNumber id="diferencia_presion" :model-value="diferencia_presion" :readonly="true" suffix=" Bar." fluid>
-      </InputNumber>
+    <div class="tarjetas">
+      <Card class="card" :pt="{ root: { id: 'calc-card' } }">
+        <template #title>Datos iniciales</template>
+        <template #content>
+          <label for="presion_inicial">Presión Inicial</label>
+          <InputNumber id="presion_inicial" v-model="presion_inicial" suffix=" Bar." fluid></InputNumber>
+          <label for="temperatura_inicial">Temperatura Inicial</label>
+          <InputNumber id="temperatura_inicial" v-model="temperatura_inicial" suffix=" ºC" fluid></InputNumber>
+          <label for="temperatura_final">Temperatura Final</label>
+          <InputNumber id="temperatura_final" v-model="temperatura_final" suffix=" ºC" fluid></InputNumber>
+        </template>
+      </Card>
+      <Card class="card" :pt="{ root: { id: 'result-card' } }">
+        <template #title>Resultado</template>
+        <template #content>
+          <h3>Presión final: <b>{{ presion_final.toFixed(2) }} Bar.</b></h3>
+          <h3>Diferencia de temperatura: <b>{{ diferencia_temperatura.toFixed(2) }} ºC.</b></h3>
+          <h3>Diferencia de presión: <b>{{ diferencia_presion.toFixed(2) }} Bar.</b></h3>
+        </template>
+      </Card>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style>
 .datos-calculo {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 12px;
+  display: flex;
+  flex-direction: row;
   padding: 2rem;
 }
 
 .datos-calculo label {
-  font-weight: bold;
+  font-size: 1.1em;
+}
+
+#calc-card {
+  margin-bottom: 1rem;
+  background: #ECE9E6;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+.p-card-body {
+  color: #000;
+}
+
+#result-card {
+  background: #ECE9E6;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 </style>
