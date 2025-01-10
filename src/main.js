84ae1ab82489b27a7bcc5bb10b716c2d40d5b9df
emartinez <enri.fc.98@gmail.com>
2025-01-10 19:35:04 +0100

    Added Tailwind to the project

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura'
+import { createApp } from 'vue'
 import App from './App.vue'
+import "./assets/tailwind.css";
+import "./style.css";
 
 const app = createApp(App)
 app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      darkModeSelector: 'light',
-    }
-  }
+  theme: 'none'
 })
 app.mount('#app')
