d4a0749a8f49bf6f295e9916bc49ffdbbdec404d
emartinez <enri.fc.98@gmail.com>
2025-01-10 21:19:55 +0100

    Added primeicons and vue router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
 import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
+import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
+
 import App from './App.vue'
-import "./assets/tailwind.css";
-import "./style.css";
+
+import CalculatorsView from '@/views/CalculatorsView.vue'
+import GayLussac from '@/components/GayLussac.vue'
+
+import './assets/tailwind.css'
+import './style.css'
+
+const routes = [
+  { path: '/', component: CalculatorsView },
+  { path: '/gay-lussac', component: GayLussac },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 const app = createApp(App)
 app.use(PrimeVue, {
   theme: 'none'
 })
+app.use(router)
 app.mount('#app')
